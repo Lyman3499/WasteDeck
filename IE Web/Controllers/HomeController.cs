6417bfceb8ddaf82747fee7e6ca37d27ddf53056
Lyman3499 <49185014+Lyman3499@users.noreply.github.com>
2020-08-28 18:17:43 +1000

    fix css, delete team in homepage

--- IE Web/Controllers/HomeController.cs
+++ IE Web/Controllers/HomeController.cs
@@ -20,6 +20,31 @@ namespace IE_Web.Controllers
             return View();
         }
 
+        public ActionResult Serivce()
+        {
+            ViewBag.Message = "Your application Service 1 page.";
+
+            return View();
+        }
+
+        public ActionResult Serivce2()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Serivce3()
+        {
+
+            return View();
+        }
+
+        public ActionResult Serivce4()
+        {
+
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
