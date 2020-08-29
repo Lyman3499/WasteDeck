be3384b42f2b0ef31de36b6ce8c2a62062e4d074
Lyman3499 <49185014+Lyman3499@users.noreply.github.com>
2020-08-29 12:43:07 +1000

    fix bug

--- IE Web/Controllers/HomeController.cs
+++ IE Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IE_Web.Models;
+
 
 namespace IE_Web.Controllers
 {
@@ -20,35 +24,35 @@ namespace IE_Web.Controllers
             return View();
         }
 
-        public ActionResult Serivce()
-        {
-            ViewBag.Message = "Your application Service 1 page.";
-
-            return View();
-        }
-
-        public ActionResult Serivce2()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Serivce3()
-        {
-
-            return View();
-        }
-
-        public ActionResult Serivce4()
-        {
-
-            return View();
-        }
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View(new Models.SendEmailViewModel());
+        }
 
+        [HttpPost]
+        public ActionResult Contact(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {               
+                    String toEmail = model.ToEmail;
+                    String fullname = model.fullname;
+                    String contents = model.Contents;
+                    Project.Utils.EmailSender es = new Project.Utils.EmailSender();
+                    es.Send(toEmail, fullname, contents);
+
+                    ViewBag.Result = "Email has been send to admin.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
             return View();
         }
     }
