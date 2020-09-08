365150d02796960b2e8fb7ce9b498ad2b003359f
Lyman3499 <49185014+Lyman3499@users.noreply.github.com>
2020-09-08 20:15:07 +1000

    add password, user: ma16;password: 5120

--- IE Web/Controllers/HomeController.cs
+++ IE Web/Controllers/HomeController.cs
@@ -10,6 +10,37 @@ using IE_Web.Models;
 
 namespace IE_Web.Controllers
 {
+    public class BasicAuthenticationAttribute : ActionFilterAttribute
+    {
+        public string BasicRealm { get; set; }
+        protected string Username { get; set; }
+        protected string Password { get; set; }
+
+        public BasicAuthenticationAttribute(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var req = filterContext.HttpContext.Request;
+            var auth = req.Headers["Authorization"];
+            if (!String.IsNullOrEmpty(auth))
+            {
+                var cred = System.Text.ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(auth.Substring(6))).Split(':');
+                var user = new { Name = cred[0], Pass = cred[1] };
+                if (user.Name == Username && user.Pass == Password) return;
+            }
+            var res = filterContext.HttpContext.Response;
+            res.StatusCode = 401;
+            res.AddHeader("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", BasicRealm ?? "Ryadel"));
+            res.End();
+        }
+    }
+
+
+    [BasicAuthenticationAttribute("ma16","5120", BasicRealm = "your-realm")]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -69,7 +100,7 @@ namespace IE_Web.Controllers
             if (ModelState.IsValid)
             {
                 try
-                {               
+                {
                     String toEmail = model.ToEmail;
                     String fullname = model.fullname;
                     String contents = model.Contents;
