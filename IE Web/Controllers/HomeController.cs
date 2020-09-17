c42744e314061a8641a7bef9fb3153ddf3273b90
Tanmay Dalvi <tdal0007@student.monash.edu>
2020-09-17 13:06:40 +1000

    References

--- IE Web/Controllers/HomeController.cs
+++ IE Web/Controllers/HomeController.cs
@@ -128,6 +128,12 @@ namespace IE_Web.Controllers
 
             return View();
         }
+        public ActionResult References()
+        {
+            ViewBag.Message = "Description Page";
+
+            return View();
+        }
 
         public ActionResult Contact()
         {
